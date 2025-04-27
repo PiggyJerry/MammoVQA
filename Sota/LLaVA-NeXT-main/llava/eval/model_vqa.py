4849c9294aab1ad7976c1d7216a32af657817d63
PiggyJerry <zjyzhujiayi55@gmail.com>
2025-04-27 13:03:58 +0800

    恢复 Sota 文件夹

---
+++ Sota/LLaVA-NeXT-main/llava/eval/model_vqa.py
@@ -0,0 +1,202 @@
+import argparse
+import torch
+import os
+import json
+from tqdm import tqdm
+import shortuuid
+
+from typing import Dict, Optional, Sequence, List
+import transformers
+import re
+
+from PIL import Image
+import math
+
+import sys
+import os
+import json
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+current_dir = os.path.dirname(os.path.abspath(__file__))
+base_dir=current_dir.split('MammoVQA')[0]+'MammoVQA'
+
+sys.path.append(os.path.join(base_dir, 'Eval'))
+sys.path.append(os.path.join(base_dir, 'Benchmark'))
+sys.path.append(os.path.join(base_dir, 'Sota/LLaVA-NeXT-main'))
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
+
+from llava.constants import IGNORE_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IMAGE_TOKEN_INDEX
+
+from Certainty_Score import certainty_score
+from Question_Answering_Score import question_answering_score
+from Utils import build_prompt
+from PIL import Image
+from Mammo_VQA_dataset import MammoVQA_image_Bench
+
+gpu_id='6'
+
+def split_list(lst, n):
+    """Split a list into n (roughly) equal-sized chunks"""
+    chunk_size = math.ceil(len(lst) / n)  # integer division
+    return [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
+
+
+def get_chunk(lst, n, k):
+    chunks = split_list(lst, n)
+    return chunks[k]
+
+def preprocess_qwen(sources, tokenizer: transformers.PreTrainedTokenizer, has_image: bool = False, max_len=2048, system_message: str = "You are a helpful assistant.") -> Dict:
+    roles = {"human": "<|im_start|>user", "gpt": "<|im_start|>assistant"}
+
+    im_start, im_end = tokenizer.additional_special_tokens_ids
+    nl_tokens = tokenizer("\n").input_ids
+    _system = tokenizer("system").input_ids + nl_tokens
+    _user = tokenizer("user").input_ids + nl_tokens
+    _assistant = tokenizer("assistant").input_ids + nl_tokens
+
+    # Apply prompt templates
+    input_ids, targets = [], []
+
+    source = sources
+    if roles[source[0]["from"]] != roles["human"]:
+        source = source[1:]
+
+    input_id, target = [], []
+    system = [im_start] + _system + tokenizer(system_message).input_ids + [im_end] + nl_tokens
+    input_id += system
+    target += [im_start] + [IGNORE_INDEX] * (len(system) - 3) + [im_end] + nl_tokens
+    assert len(input_id) == len(target)
+    for j, sentence in enumerate(source):
+        role = roles[sentence["from"]]
+        if has_image and sentence["value"] is not None and "<image>" in sentence["value"]:
+            num_image = len(re.findall(DEFAULT_IMAGE_TOKEN, sentence["value"]))
+            texts = sentence["value"].split('<image>')
+            _input_id = tokenizer(role).input_ids + nl_tokens 
+            for i,text in enumerate(texts):
+                _input_id += tokenizer(text).input_ids 
+                if i<len(texts)-1:
+                    _input_id += [IMAGE_TOKEN_INDEX] + nl_tokens
+            _input_id += [im_end] + nl_tokens
+            assert sum([i==IMAGE_TOKEN_INDEX for i in _input_id])==num_image
+        else:
+            if sentence["value"] is None:
+                _input_id = tokenizer(role).input_ids + nl_tokens
+            else:
+                _input_id = tokenizer(role).input_ids + nl_tokens + tokenizer(sentence["value"]).input_ids + [im_end] + nl_tokens
+        input_id += _input_id
+        if role == "<|im_start|>user":
+            _target = [im_start] + [IGNORE_INDEX] * (len(_input_id) - 3) + [im_end] + nl_tokens
+        elif role == "<|im_start|>assistant":
+            _target = [im_start] + [IGNORE_INDEX] * len(tokenizer(role).input_ids) + _input_id[len(tokenizer(role).input_ids) + 1 : -2] + [im_end] + nl_tokens
+        else:
+            raise NotImplementedError
+        target += _target
+
+    input_ids.append(input_id)
+    targets.append(target)
+    input_ids = torch.tensor(input_ids, dtype=torch.long)
+    targets = torch.tensor(targets, dtype=torch.long)
+    return input_ids
+
+def eval_model(args):
+    
+    # Model
+    disable_torch_init()
+    import warnings
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", message="for .*: copying from a non-meta parameter")
+        model_path = os.path.expanduser(args.model_path)
+        model_name = get_model_name_from_path(model_path)
+        tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name,device_map={"":'cuda:'+gpu_id})
+        model=model.to('cuda:'+gpu_id)
+
+    with open(os.path.join(base_dir, 'Benchmark/MammoVQA-Image-Bench.json'), 'r') as f:
+        data = json.load(f)
+
+    MammoVQAData=MammoVQA_image_Bench(data,base_dir)
+    eval_dataloader = DataLoader(MammoVQAData, batch_size=1, shuffle=False)
+    results = {}
+    for images, qas_questions, img_ids in tqdm(eval_dataloader):
+        
+        image_files = [images[0]]
+        qas_qs="<image>"*len(image_files)+qas_questions[0]
+        qas_conversation=[
+                {
+                    "from": "human",
+                    "value": qas_qs
+                }
+            ]
+
+        args.conv_mode = "qwen_1_5"
+
+        conv = conv_templates[args.conv_mode].copy()
+        conv.append_message(conv.roles[0], qas_qs)
+        conv.append_message(conv.roles[1], None)
+
+        qas_input_ids = preprocess_qwen([qas_conversation[0],{'from': 'gpt','value': None}], tokenizer, has_image=True).to('cuda:'+gpu_id)
+        qas_img_num = list(qas_input_ids.squeeze()).count(IMAGE_TOKEN_INDEX)
+
+        image_tensors = []
+        for image_file in image_files:
+            image = Image.open(image_file)
+            image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values']
+            image_tensors.append(image_tensor.half().to('cuda:'+gpu_id))
+
+        qas_stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+        with torch.inference_mode():
+            qas_output = model.generate(
+                qas_input_ids,
+                images=image_tensors,
+                do_sample=True if args.temperature > 0 else False,
+                temperature=args.temperature,
+                top_p=args.top_p,
+                num_beams=args.num_beams,
+                # no_repeat_ngram_size=3,
+                max_new_tokens=1024,
+                use_cache=True,
+                output_scores=True,return_dict_in_generate=True)
+            
+        qas_output_ids,qas_logits=qas_output.sequences,qas_output.scores
+
+        qas_outputs = tokenizer.batch_decode(qas_output_ids, skip_special_tokens=True)[0]
+        qas_outputs = qas_outputs.strip()
+        if qas_outputs.endswith(qas_stop_str):
+            qas_outputs = qas_outputs[:-len(qas_stop_str)]
+        qas_outputs = qas_outputs.strip()
+        
+        for qas_answer, qas_question, img_id in zip(qas_outputs, qas_questions, img_ids):
+
+            target,question_type=data[str(img_id)]['Answer'],data[str(img_id)]['Question type']
+            qas_answer=qas_answer.replace('<unk>','').replace('\u200b','').replace('\n','').strip()
+            result = dict()
+            result['qas_question']=qas_question
+            result['qas_answer']=qas_answer
+            results[str(img_id)] = result
+        
+    with open(base_dir+'/Result/LLAVA-NeXT-interleave.json', 'w') as f:
+        json.dump(results, f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", type=str, default=f"{base_dir}/LLM/llava-next-interleave-qwen-7b")
+    parser.add_argument("--model-base", type=str, default=None)
+    # parser.add_argument("--image-folder", type=str, default="")
+    parser.add_argument("--extra-prompt", type=str, default="")
+    # parser.add_argument("--question-file", type=str, default="tables/question.jsonl")
+    # parser.add_argument("--answers-file", type=str, default="answer.jsonl")
+    parser.add_argument("--conv-mode", type=str, default="llava_v1")
+    parser.add_argument("--num-chunks", type=int, default=1)
+    parser.add_argument("--chunk-idx", type=int, default=0)
+    parser.add_argument("--temperature", type=float, default=0.2)
+    parser.add_argument("--top_p", type=float, default=None)
+    parser.add_argument("--num_beams", type=int, default=1)
+    parser.add_argument("--test_size", type=int, default=10000000)
+    args = parser.parse_args()
+
+    eval_model(args)
---
+++ Sota/RadFM-main/Quick_demo/test_exam.py
@@ -0,0 +1,164 @@
+import tqdm.auto as tqdm
+import torch.nn.functional as F
+from typing import Optional, Dict, Sequence
+from typing import List, Optional, Tuple, Union
+import transformers
+from dataclasses import dataclass, field
+from Model.RadFM.multimodality_model import MultiLLaMAForCausalLM
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaTokenizer
+from torchvision import transforms
+from PIL import Image   
+import torch.nn as nn
+
+import sys
+import os
+import json
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+current_dir = os.path.dirname(os.path.abspath(__file__))
+base_dir=current_dir.split('MammoVQA')[0]+'MammoVQA'
+sys.path.append(os.path.join(base_dir, 'Eval'))
+sys.path.append(os.path.join(base_dir, 'Benchmark'))
+
+from Certainty_Score import certainty_score
+from Question_Answering_Score import question_answering_score
+from PIL import Image
+from Mammo_VQA_dataset import MammoVQA_exam_Bench
+
+def get_tokenizer(tokenizer_path, max_img_size = 100, image_num = 32):
+    '''
+    Initialize the image special tokens
+    max_img_size denotes the max image put length and image_num denotes how many patch embeddings the image will be encoded to 
+    '''
+    if isinstance(tokenizer_path,str):
+        image_padding_tokens = []
+        text_tokenizer = LlamaTokenizer.from_pretrained(
+            tokenizer_path,
+        )
+        special_token = {"additional_special_tokens": ["<image>","</image>"]}
+        for i in range(max_img_size):
+            image_padding_token = ""
+            
+            for j in range(image_num):
+                image_token = "<image"+str(i*image_num+j)+">"
+                image_padding_token = image_padding_token + image_token
+                special_token["additional_special_tokens"].append("<image"+str(i*image_num+j)+">")
+            image_padding_tokens.append(image_padding_token)
+            text_tokenizer.add_special_tokens(
+                special_token
+            )
+            ## make sure the bos eos pad tokens are correct for LLaMA-like models
+            text_tokenizer.pad_token_id = 0
+            text_tokenizer.bos_token_id = 1
+            text_tokenizer.eos_token_id = 2    
+    
+    return  text_tokenizer,image_padding_tokens    
+
+def combine_and_preprocess(qas_question,image_list,image_padding_tokens):
+    
+    transform = transforms.Compose([                        
+                transforms.RandomResizedCrop([512,512],scale=(0.8, 1.0), interpolation=transforms.InterpolationMode.BICUBIC),
+                transforms.ToTensor(),
+            ])
+    images  = []
+    qas_new_qestions = [_ for _ in qas_question]
+    # cs_new_qestions = [_ for _ in cs_question]
+    padding_index = 0
+    for img in image_list:
+        img_path = img['img_path']
+        position = img['position']
+        
+        image = Image.open(img_path).convert('RGB')   
+        image = transform(image)
+        image = image.unsqueeze(0).unsqueeze(-1) # c,w,h,d
+        
+        ## pre-process the img first
+        target_H = 512 
+        target_W = 512 
+        target_D = 4 
+        # This can be different for 3D and 2D images. For demonstration we here set this as the default sizes for 2D images. 
+        images.append(torch.nn.functional.interpolate(image, size = (target_H,target_W,target_D)))
+        
+        ## add img placeholder to text
+        qas_new_qestions[position] = "<image>"+ image_padding_tokens[padding_index] +"</image>" + qas_new_qestions[position]
+        # cs_new_qestions[position] = "<image>"+ image_padding_tokens[padding_index] +"</image>" + cs_new_qestions[position]
+        padding_index +=1
+    
+    vision_x = torch.cat(images,dim = 1).unsqueeze(0) #cat tensors and expand the batch_size dim
+    qas_text = ''.join(qas_new_qestions) 
+    # cs_text = ''.join(cs_new_qestions) 
+    return qas_text, vision_x, 
+    
+    
+def main():
+    print("Setup tokenizer")
+    text_tokenizer,image_padding_tokens = get_tokenizer(os.path.join(current_dir, 'Language_files'))
+    print("Finish loading tokenizer")
+    
+    ### Initialize a simple case for demo ###
+    
+    print("Setup Model")
+    from accelerate import load_checkpoint_and_dispatch, infer_auto_device_map
+
+    # model.load_state_dict(ckpt)
+    model = MultiLLaMAForCausalLM(
+        lang_model_path=os.path.join(current_dir, 'Language_files'), 
+    )
+    print("Finish loading model")
+    
+    # num_gpus=2
+    
+    max_memory = {3: "40GiB", 4: "40GiB"}
+    device_map = infer_auto_device_map(model, max_memory=max_memory, no_split_module_classes=["MultiLLaMAForCausalLM"])
+
+    model = load_checkpoint_and_dispatch(
+        model, checkpoint=os.path.join(current_dir, 'pytorch_model.bin'), device_map=device_map,
+    )
+
+    model.eval()
+    
+    with open(os.path.join(base_dir, 'Benchmark/MammoVQA-Exam-Bench.json'), 'r') as f:
+        data = json.load(f)
+
+    MammoVQAData=MammoVQA_exam_Bench(data,base_dir)
+    eval_dataloader = DataLoader(MammoVQAData, batch_size=1, shuffle=False)
+    results = {}
+    with torch.no_grad():
+        for images, qas_questions, img_ids in tqdm(eval_dataloader):
+            image =[
+                    {
+                        'img_path': images[0][i],
+                        'position': i  # 将 position 设置为当前索引 i
+                    }
+                    for i in range(len(images[0]))
+                ] 
+                
+            qas_text,vision_x = combine_and_preprocess(qas_questions[0],image,image_padding_tokens)    
+            qas_lang_x = text_tokenizer(
+                qas_text, max_length=2048, truncation=True, return_tensors="pt"
+            )['input_ids']
+           
+            qas_lang_x = qas_lang_x.to('cuda')
+            vision_x = vision_x.to('cuda')
+
+            qas_generation = model.generate(qas_lang_x, vision_x)
+            qas_batch_output_tokens=qas_generation.sequences
+            
+            qas_answers = text_tokenizer.batch_decode(qas_batch_output_tokens, skip_special_tokens=True)
+            
+            for qas_answer, qas_question, img_id in zip(qas_answers, qas_questions, img_ids):
+                qas_answer=qas_answer.replace('<unk>','').replace('\u200b','').replace('\n','').strip()
+                
+                result = dict()
+                result['qas_question']=qas_question
+                result['qas_answer']=qas_answer
+                results[str(img_id)] = result
+        with open(base_dir+'/Result/RadFM-Exam.json', 'w') as f:
+            json.dump(results, f, indent=4)
+
+
+    
+if __name__ == "__main__":
+    main()
+       
